182eb8a4048080b07077e4ee649210140e4998e4
Marcel <marcel@udiocollective.com>
2020-11-27 15:00:21 +0000

    visual update

--- src/components/materials/materialitem.js
+++ src/components/materials/materialitem.js
@@ -22,10 +22,12 @@ class MaterialItem extends Component {
     return (
       <div key="{this.state.index + 1}" className="material-item">
         <h1>{this.state.data.name}</h1>
+        <div className="material-description">{this.state.data.description}</div>
         <div className='material-information-headline'>Notes</div>
         <div className='material-notes'>{this.state.data.notes}</div>
         {this.state.data.datasheet_url && (<div className='material-ds'><a href="{this.state.data.datasheet_url}">Link to datasheet</a></div>)}
         {this.state.data.technicalinfosheet_url && (<div className='material-ds'><a href="{this.state.data.technicalinfosheet_url}">Link to technical info sheet</a></div>)}
+        <div className="material-uuid">{this.state.data.uuid}</div>
       </div>
     )
   }
