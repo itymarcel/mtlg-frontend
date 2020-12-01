0e2b81f336ece42a3a9acd49a10fff964c2ff013
Marcel <marcel@udiocollective.com>
2020-12-01 10:40:24 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MaterialListContainer from './containers/materials/materiallistcontainer'
 function App() {
   return (
     <div className='app'>
-      <MaterialListContainer />
+      <MaterialListContainer materialUuids={['18952eae-2b35-11eb-adc1-0242ac120002', '3999fec2-30bc-11eb-adc1-0242ac120002']} />
     </div>
   );
 }
--- src/containers/materials/materiallistcontainer.js
+++ src/containers/materials/materiallistcontainer.js
@@ -2,18 +2,27 @@ import React, { Component } from "react";
 import MaterialList from '../../components/materials/materiallist';
 
 class MaterialListContainer extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = { 
       loading: true,
+      materialUuids: props.materialUuids,
       materials: [] 
     };
   }
 
   async componentDidMount() {
     try {
-      // const response = await fetch(`http://localhost:4000`);
-      const response = await fetch(`https://mtlg.herokuapp.com/`);
+      const settings = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ materialUuids: this.state.materialUuids })
+      };
+      // const response = await fetch(`http://localhost:4000/materials/getmaterials`, settings);
+      const response = await fetch(`https://mtlg.herokuapp.com/`, settings);
+      console.log('response: ', response);
       const response_json = await response.json();
       console.log(response_json);
       this.setState({ loading: false, materials : response_json });
